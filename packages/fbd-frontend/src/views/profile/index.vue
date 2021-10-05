f247fbdd39bd5efc84cec7e75701b3a6d9c4d33d
joeseph <j50607@yahoo.com.tw>
2021-10-05 16:55:58 +0800

    hotfix(sideBar) 修正VIP頁進不去
    問題：
    
    1.修正VIP頁無法從我的進去
    
    調整：
    
    1.調整完成

--- packages/fbd-frontend/src/views/profile/index.vue
+++ packages/fbd-frontend/src/views/profile/index.vue
@@ -303,7 +303,6 @@ export default {
     const goPage = (page) => {
       switch (page) {
         case '/profile/report':
-        case '/profile/vip':
         case '/profile/verify':
           window.$vue.$message.info(t('common_comingSoon'));
           break;
