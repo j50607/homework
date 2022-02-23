f5cfdcda72852d1545bb557451980d3896032e31
joeseph <j50607@yahoo.com.tw>
2022-02-23 11:26:20 +0800

    first commit

---
+++ packages/admin/vue.config.js
@@ -0,0 +1,19 @@
+const { defineConfig } = require('@vue/cli-service')
+const path = require('path')
+const adminNodeModules = path.resolve(__dirname, './node_modules')
+const sharedNodeModules = path.resolve(__dirname, '../shared/node_modules')
+const adminSrc = path.resolve(__dirname, 'src')
+const sharedSrc = path.resolve(__dirname, '../shared/src')
+
+module.exports = defineConfig({
+  transpileDependencies: true,
+  configureWebpack: {
+    resolve: {
+      modules: [adminNodeModules, sharedNodeModules], // this will allow `admin` module to import NPM packages from `shared`
+      alias: { 
+        '@shared': sharedSrc,
+        '@admin': adminSrc
+      }
+    }
+  }
+})
